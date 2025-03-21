628b26a75250b43cf4c34a605cad3da2d085ab83
Kiameow <zjzootl@qq.com>
2025-03-21 09:39:51 +0800

    test for num=1, dim=1

--- model.py
+++ model.py
@@ -25,8 +25,8 @@ myVQVAE = VQVAE(
         (2, 4, 1, 1, 0),  # Second upsampling: 64x64 → 128x128
         (2, 4, 1, 1, 0),  # Third upsampling: 128x128 → 256x256
     ),
-    num_embeddings=32,
-    embedding_dim=2
+    num_embeddings=1,
+    embedding_dim=1
 )
 
 # diffusion.py
