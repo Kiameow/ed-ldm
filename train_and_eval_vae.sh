2864efe9b26af9345fc15dbfd8340eb7d06ec474
Kiameow <zjzootl@qq.com>
2025-03-26 10:00:21 +0800

    update train script

--- train_and_eval_vae.sh
+++ train_and_eval_vae.sh
@@ -1,6 +1,6 @@
-python vqvae_train.py --embedding_dim 1 --num_embeddings 2 --cuda_num $1
-python vqvae_train.py --embedding_dim 1 --num_embeddings 4 --cuda_num $1
-python vqvae_train.py --embedding_dim 1 --num_embeddings 8 --cuda_num $1
-python vqvae_eval.py  --embedding_dim 1 --num_embeddings 2 --cuda_num $1
-python vqvae_eval.py  --embedding_dim 1 --num_embeddings 4 --cuda_num $1
-python vqvae_eval.py  --embedding_dim 1 --num_embeddings 8 --cuda_num $1
+python vqvae_train.py --embedding_dim 16 --num_embeddings 512 --cuda_num $1
+# python vqvae_train.py --embedding_dim 16 --num_embeddings 1024 --cuda_num $1
+# python vqvae_train.py --embedding_dim 1 --num_embeddings 8 --cuda_num $1
+python vqvae_eval.py  --embedding_dim 16 --num_embeddings 512 --cuda_num $1
+# python vqvae_eval.py  --embedding_dim 1 --num_embeddings 4 --cuda_num $1
+# python vqvae_eval.py  --embedding_dim 1 --num_embeddings 8 --cuda_num $1
